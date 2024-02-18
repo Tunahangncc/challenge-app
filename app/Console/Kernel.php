d5011aaa72409765851fdb07edbfb385ce810b5c
Tunahan Genç <83228688+Tunahangncc@users.noreply.github.com>
2024-02-19 00:31:24 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:cancellation-of-expired-subscriptions')->everyMinute();
+        $schedule->command('app:cancellation-of-expired-subscriptions')->daily();
     }
 
     protected function commands(): void
